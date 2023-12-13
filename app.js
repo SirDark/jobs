22d6103373f5b76894be3d0a1b6fe304a9fc17db
SirDark <gergelykevin123@gmail.com>
2023-12-13 21:00:18 +0100

    registering

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const jobRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect')
 
 app.use(express.json());
 // extra packages
+
+// connectDB
 
 // routes
 app.use('/api/v1/auth', authRouter)
@@ -23,6 +26,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const UserSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, 'please provide a username'],
+        minlength: 3,
+        maxlength: 50
+    },
+    email:{
+        type:String,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'please provide a valid email'
+        ],
+        unique: true,
+    },
+    password:{
+        type: String,
+        required:[true,"please provide a password"],
+        minlength: 6,
+    }
+})
+
+UserSchema.pre('save', async function(next){ //use function so "this" keyword points to document
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+UserSchema.methods.createJWTSign = function(){
+    return jwt.sign({userId: this._id, name: this.name},
+        process.env.JWT_SECRET,
+        {expiresIn: '30d'})
+}
+
+module.exports = mongoose.model('User', UserSchema)
