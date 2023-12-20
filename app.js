1eca556c1e2cc42f11e1894d7c9074e52da78525
SirDark <gergelykevin123@gmail.com>
2023-12-20 10:26:48 +0100

    security

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+//routes
 const authRouter = require('./routes/auth')
 const jobRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const connectDB = require('./db/connect')
-const authenticateUser = require('./middleware/authentication')
+const authenticateUser = require('./middleware/authentication');
 
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowMs: 15*60*1000, //15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser,jobRouter)
